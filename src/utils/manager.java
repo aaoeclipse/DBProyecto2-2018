73d360858b1d70ccc7e50ab985362daada3c7cb1
Unknown <santiago.paiz@gmail.com>
2018-04-28 08:58:43 -0600

    utils

--- src/utils/manager.java
+++ src/utils/manager.java
@@ -1,4 +1,16 @@
 package utils;
 
+import objects.Columna;
+import objects.Tabla;
+import objects.Valor;
+
+import java.util.ArrayList;
+
 public class manager {
+    private ArrayList<Columna> columnas;
+    public String[][] toTable(Tabla table){
+        columnas = table.getColumnas();
+        String[][] columnsByRow;
+        columnsByRow = new String [columnas.getSize()][];
+    }
 }
